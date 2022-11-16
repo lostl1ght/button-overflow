292a54ebfc6f72dbc57b8f1158f254f9d9724c30
Constantine Bulany <61948252+lostl1ght@users.noreply.github.com>
2022-11-16 20:50:02 +0300

    Add django

---
+++ app/views.py
@@ -0,0 +1,12 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+def ask(request):
+    return render(request, 'ask.html')
+
+def question(request, id):
+    return render(request, 'question.html')
